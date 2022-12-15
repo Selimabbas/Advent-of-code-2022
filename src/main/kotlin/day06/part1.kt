649bee93e3b3286a179dce4d6f96c8008a58538d
selim.abbassaid <selim.abbassaid@adevinta.com>
2022-12-15 12:02:52 +0100

    day 6

---
+++ src/main/kotlin/day06/part1.kt
@@ -0,0 +1,20 @@
+package day06
+
+import java.io.File
+
+fun main() {
+    File("src/main/kotlin/day06/input.txt")
+        .readLines()
+        .forEach {
+            for (i in it.indices) {
+                val end = i + 3
+                val substring = it.substring(i..end).toCharArray()
+                println("${it.substring(i..end)} / ${substring.distinct()}")
+                if (substring.size == substring.distinct().size) {
+                    println(end + 1)
+                    return
+                }
+            }
+        }
+}
+
